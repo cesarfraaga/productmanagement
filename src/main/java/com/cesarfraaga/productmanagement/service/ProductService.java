3a53c405e336a3b425770c425511da9c4dc5edbe
César Fraga <cesarfraaga@gmail.com>
2024-11-19 11:19:56 -0300

    feat: create ProductService

---
+++ src/main/java/com/cesarfraaga/productmanagement/service/ProductService.java
@@ -0,0 +1,43 @@
+package com.cesarfraaga.productmanagement.service;
+
+import com.cesarfraaga.productmanagement.entity.Product;
+import com.cesarfraaga.productmanagement.repository.ProductRepository;
+import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private ProductRepository repository;
+
+    public Product save(Product product) {
+        if (product.getName() == null || product.getPrice() == null)
+            System.out.println("Informações insuficientes.");
+        if (product.getPrice() < 0)
+            System.out.println("O valor do produto tem que ser maior que 0");
+        return repository.save(product);
+    }
+
+    public Product update(Product product) {
+        if (repository.existsById(product.getId())) {
+            return repository.save(product);
+        } else {
+            throw new ResourceNotFoundException("Não existe um produto com ID " + product.getId() + ".");
+        }
+    }
+
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    public Product findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+}
