727108b66a064f2dcd931e801f7d415c3b2e73ef
César Fraga <cesarfraaga@gmail.com>
2024-11-19 11:18:36 -0300

    feat: create ProductController with initial structure

---
+++ src/main/java/com/cesarfraaga/productmanagement/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.cesarfraaga.productmanagement.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+
+
+}
