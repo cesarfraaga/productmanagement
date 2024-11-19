be919e35b1f4dfb56f78026501c1dba64833d577
César Fraga <cesarfraaga@gmail.com>
2024-11-19 11:17:21 -0300

    chore: change variable price to Double type

--- src/main/java/com/cesarfraaga/productmanagement/entity/Product.java
+++ src/main/java/com/cesarfraaga/productmanagement/entity/Product.java
@@ -16,7 +16,7 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    private double price;
+    private Double price;
     private String description;
 
 }
