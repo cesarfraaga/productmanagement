53f8d8f4dfbfd68594df0bddc83d8cb65f6d9bee
César Fraga <cesarfraaga@gmail.com>
2024-11-21 11:03:44 -0300

    feat: create findById method and update saveProduct with DTO pattern

--- src/main/java/com/cesarfraaga/productmanagement/controller/ProductController.java
+++ src/main/java/com/cesarfraaga/productmanagement/controller/ProductController.java
@@ -3,11 +3,9 @@ package com.cesarfraaga.productmanagement.controller;
 import com.cesarfraaga.productmanagement.dto.ProductDTO;
 import com.cesarfraaga.productmanagement.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/product")
@@ -17,9 +15,15 @@ public class ProductController {
     private ProductService service;
 
     @PostMapping("/save")
-    public ResponseEntity<ProductDTO> saveProduct(@RequestBody ProductDTO product) {
-        service.save(product);
-        return null;
+    public ResponseEntity<ProductDTO> saveProduct(@RequestBody ProductDTO productDTO) {
+        ProductDTO savedProduct = service.save(productDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedProduct);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+        ProductDTO productDTO = service.findById(id);
+        return ResponseEntity.ok(productDTO);
     }
 
 }
