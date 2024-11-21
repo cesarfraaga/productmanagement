439c7de3ec0272587f53ba0c7797329f06343830
César Fraga <cesarfraaga@gmail.com>
2024-11-21 11:05:15 -0300

    chore: ProductRepository has been updated with DTO pattern

--- src/main/java/com/cesarfraaga/productmanagement/service/ProductService.java
+++ src/main/java/com/cesarfraaga/productmanagement/service/ProductService.java
@@ -3,7 +3,9 @@ package com.cesarfraaga.productmanagement.service;
 import com.cesarfraaga.productmanagement.dto.ProductDTO;
 import com.cesarfraaga.productmanagement.entity.Product;
 import com.cesarfraaga.productmanagement.repository.ProductRepository;
+import com.cesarfraaga.productmanagement.util.ProductMapper;
 import org.apache.velocity.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,37 +13,47 @@ import java.util.List;
 @Service
 public class ProductService {
 
-    private ProductRepository repository;
+    private ProductRepository productRepository;
+    private ProductMapper productMapper;
 
-    public Product save(ProductDTO product) {
-        if (product.getName() == null || product.getPrice() == null)
+    @Autowired
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    public ProductDTO save(ProductDTO dto) {
+
+        Product product = productMapper.toEntity(dto);
+        product = productRepository.save(product);
+        return productMapper.toDTO(product);
+
+        /*if (product.getName() == null || product.getPrice() == null)
             System.out.println("Informações insuficientes.");
         if (product.getPrice() < 0)
             System.out.println("O valor do produto tem que ser maior que 0");
-
-        // Criar um mapper para converter ProductDTO em Product e vice-versa
-        return null;
-        //return repository.save(product);
+        return repository.save(product);*/
     }
 
     public Product update(Product product) {
-        if (repository.existsById(product.getId())) {
-            return repository.save(product);
+        if (productRepository.existsById(product.getId())) {
+            return productRepository.save(product);
         } else {
             throw new ResourceNotFoundException("Não existe um produto com ID " + product.getId() + ".");
         }
     }
 
     public void deleteById(Long id) {
-        repository.deleteById(id);
+        productRepository.deleteById(id);
     }
 
-    public Product findById(Long id) {
-        return repository.findById(id).orElse(null);
+    public ProductDTO findById(Long id) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product not Found"));
+        return productMapper.toDTO(product);
     }
 
     public List<Product> findAll() {
-        return repository.findAll();
+        return productRepository.findAll();
     }
 
 }
