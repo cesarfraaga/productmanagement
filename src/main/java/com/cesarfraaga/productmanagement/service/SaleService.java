3087fb85a794b096b907a7d67a5087305f5812e1
César Fraga <cesarfraaga@gmail.com>
2024-11-29 12:48:09 -0300

    feat|: create basic structure to Sale

---
+++ src/main/java/com/cesarfraaga/productmanagement/service/SaleService.java
@@ -0,0 +1,11 @@
+package com.cesarfraaga.productmanagement.service;
+
+import com.cesarfraaga.productmanagement.repository.SaleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SaleService {
+
+    SaleRepository repository;
+
+}
