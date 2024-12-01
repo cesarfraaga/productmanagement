431e4e1c422d6ec5ca54b8f522066859011adedd
César Fraga <cesarfraaga@gmail.com>
2024-12-01 02:11:24 -0300

    chore: modify Lombok annotations

--- src/main/java/com/cesarfraaga/productmanagement/dto/ProductDTO.java
+++ src/main/java/com/cesarfraaga/productmanagement/dto/ProductDTO.java
@@ -1,12 +1,10 @@
 package com.cesarfraaga.productmanagement.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
-@Setter
-@Getter
+@Data
 public class ProductDTO {
     private Long id;
     private String name;
