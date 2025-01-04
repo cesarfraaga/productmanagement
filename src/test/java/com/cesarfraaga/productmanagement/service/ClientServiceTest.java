7480eddd2e3db26974bff450f5119e5ee5f8de48
César Fraga <cesarfraaga@gmail.com>
2025-01-04 11:10:41 -0300

    test: refactor ClientService tests with new cases and nested structure

--- src/test/java/com/cesarfraaga/productmanagement/service/ClientServiceTest.java
+++ src/test/java/com/cesarfraaga/productmanagement/service/ClientServiceTest.java
@@ -5,6 +5,8 @@ import com.cesarfraaga.productmanagement.entity.Client;
 import com.cesarfraaga.productmanagement.exception.ResourceNotFoundException;
 import com.cesarfraaga.productmanagement.repository.ClientRepository;
 import com.cesarfraaga.productmanagement.util.ClientMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,45 +31,64 @@ public class ClientServiceTest {
     @Spy
     private ClientMapper clientMapper;
 
-    @Test
-    void shouldSaveClientSuccessfully() {
-        Client client = new Client(1L, "César", "12345", "20/01/2003");
-        ClientDTO clientDTO = new ClientDTO(1L, "César", "12345", "20/01/2003");
+    @Nested
+    @DisplayName("Save tests")
+    class MethodSaveTests {
+        @Test
+        void shouldSaveClientSuccessfully() {
+            Client client = new Client(1L, "César", "12345", "20/01/2003");
+            ClientDTO clientDTO = new ClientDTO(1L, "César", "12345", "20/01/2003");
 
-        when(clientRepository.save(any())).thenReturn(client);
-        when(clientMapper.clientToEntity(any(ClientDTO.class))).thenReturn(client);
-        when(clientMapper.clientToDTO(any(Client.class))).thenReturn(clientDTO);
+            when(clientRepository.save(any())).thenReturn(client);
+            when(clientMapper.clientToEntity(any(ClientDTO.class))).thenReturn(client);
+            when(clientMapper.clientToDTO(any(Client.class))).thenReturn(clientDTO);
 
-        ClientDTO response = clientService.save(clientDTO);
+            ClientDTO response = clientService.save(clientDTO);
 
-        assertEquals(clientDTO.getId(), response.getId());  //garante que o valor <response.getId()> é igual ao valor esperado <clientDTO.getId()>
-        assert response.getName().equals(clientDTO.getName());
-        assert response.getCPF().equals(clientDTO.getCPF());
-        assert response.getBirthDay().equals(clientDTO.getBirthDay());
+            assertEquals(clientDTO.getId(), response.getId());  //garante que o valor <response.getId()> é igual ao valor esperado <clientDTO.getId()>
+            assert response.getName().equals(clientDTO.getName());
+            assert response.getCPF().equals(clientDTO.getCPF());
+            assert response.getBirthDay().equals(clientDTO.getBirthDay());
 
+        }
+
+        @Test
+        void shouldThrowExceptionWhenClientNameIsNull() {
+            ClientDTO clientDTO = new ClientDTO(1L, null, "12345", "20/01/2003");
+
+            assertThrows(ResourceNotFoundException.class, () -> clientService.save(clientDTO));
+            verify(clientMapper, never()).clientToDTO(any());
+        }
+
+        //Posso fazer um teste parametrizado mudando apenas o valor da variável name
+        @Test
+        void shouldThrowExceptionWhenClientNameIsBlank() {
+            ClientDTO clientDTO = new ClientDTO(1L, "", "12345", "20/01/2003");
+
+            assertThrows(ResourceNotFoundException.class, () -> clientService.save(clientDTO)); //assegura que a chamada do método <clientService.save()> com <esse parametro> <lanca uma ResourceNotFoundException>
+            verify(clientMapper, never()).clientToDTO(any());   //verifica que o <mapper> <nunca> <chama esse método>
+        }
+
+        @Test
+        void shouldThrowExceptionWhenClientNameLengthIsLessThanTwo() {
+            ClientDTO clientDTO = new ClientDTO(1L, "A", "12345", "20/01/2003");
+
+            assertThrows(IllegalArgumentException.class, () -> clientService.save(clientDTO));
+            verify(clientMapper, never()).clientToDTO(any());
+        }
+
+        @Test
+        void shouldThrowExceptionWhenClientNameIsLongerThanFifty() {
+            ClientDTO clientDTO = new ClientDTO(1L, "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY", "12345", "20/01/2003");
+
+            assertThrows(IllegalArgumentException.class, () -> clientService.save(clientDTO));
+            verify(clientMapper, never()).clientToDTO(any());
+        }
     }
 
-    @Test
-    void shouldThrowExceptionWhenClientNameIsNull() {
-        Client client = new Client(1L, null, "12345", "20/01/2003");
-        ClientDTO clientDTO = new ClientDTO(1L, null, "12345", "20/01/2003");
+    @Nested
+    @DisplayName("Update Tests")
+    class MethodUpdateTests {
 
-        when(clientRepository.save(any())).thenReturn(client);
-        when(clientMapper.clientToEntity(any(ClientDTO.class))).thenReturn(client);
-
-        assertThrows(ResourceNotFoundException.class, () -> clientService.save(clientDTO));
-        verify(clientMapper, never()).clientToDTO(any());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenClientNameIsBlank() {
-        Client client = new Client(1L, "", "12345", "20/01/2003");
-        ClientDTO clientDTO = new ClientDTO(1L, "", "12345", "20/01/2003");
-
-        when(clientRepository.save(any())).thenReturn(client);
-        when(clientMapper.clientToEntity(any(ClientDTO.class))).thenReturn(client);
-
-        assertThrows(ResourceNotFoundException.class, () -> clientService.save(clientDTO)); //assegura que a chamada do método <clientService.save()> com <esse parametro> <lanca uma ResourceNotFoundException>
-        verify(clientMapper, never()).clientToDTO(any());   //verifica que o <mapper> <nunca> <chama esse método>
     }
 }
