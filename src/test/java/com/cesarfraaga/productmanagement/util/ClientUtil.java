29197aeacf2bd4c5c729d669c73760cd762aa2a4
César Fraga <cesarfraaga@gmail.com>
2025-01-06 11:48:33 -0300

    refactor: implement reusable object instances within methods

---
+++ src/test/java/com/cesarfraaga/productmanagement/util/ClientUtil.java
@@ -0,0 +1,48 @@
+package com.cesarfraaga.productmanagement.util;
+
+import com.cesarfraaga.productmanagement.dto.ClientDTO;
+import com.cesarfraaga.productmanagement.entity.Client;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ClientUtil {
+
+    public static Client buildValidClient() {
+        return new Client(1L, "César", "12345", "20/01/2003");
+    }
+
+    public static ClientDTO buildValidClientDTO() {
+        return new ClientDTO(1L, "César", "12345", "20/01/2003");
+    }
+
+    public static ClientDTO buildClientDTOWithNullName() {
+        return new ClientDTO(1L, null, "123456", "20/01/2003");
+    }
+
+    public static ClientDTO buildClientDTOWithBlankName() {
+        return new ClientDTO(1L, "", "12345", "20/01/2003");
+    }
+
+    public static ClientDTO buildClientDTOWithNameLengthIsLessThanTwo(){
+        return new ClientDTO(1L, "A", "12345", "20/01/2003");
+    }
+
+    public static ClientDTO buildClientDTOWithNameLengthIsLongerThanFifty(){
+        return new ClientDTO(1L, "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXY", "12345", "20/01/2003");
+    }
+
+    public static ClientDTO buildClientDTOWithNullCpf() {
+        return new ClientDTO(1L, "César", null, "20/01/2003");
+    }
+    public static ClientDTO buildClientDTOWithEmptyCpf() {
+        return new ClientDTO(1L, "César", "", "20/01/2003");
+    }
+
+    public static ClientDTO buildClientDTOWithNullBirthDay() {
+        return new ClientDTO(1L, "César", "123456", null);
+    }
+    public static ClientDTO buildClientDTOWithEmptyBirthDay() {
+        return new ClientDTO(1L, "César", "123456", "");
+    }
+}
