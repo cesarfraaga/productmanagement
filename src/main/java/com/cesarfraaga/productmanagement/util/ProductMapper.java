8565cdd667a359060a46f6e6822e05b978ce4448
César Fraga <cesarfraaga@gmail.com>
2025-02-15 08:57:20 -0300

    style: fix indentation in product mapper

--- src/main/java/com/cesarfraaga/productmanagement/util/ProductMapper.java
+++ src/main/java/com/cesarfraaga/productmanagement/util/ProductMapper.java
@@ -6,6 +6,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
-    ProductDTO toDTO(Product product);
     Product toEntity(ProductDTO dto);
+
+    ProductDTO toDTO(Product product);
 }
