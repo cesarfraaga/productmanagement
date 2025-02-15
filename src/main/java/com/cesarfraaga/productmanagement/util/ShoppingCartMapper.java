7b508c1ab47ce88dca3136bdb34e5d4354b3bd46
César Fraga <cesarfraaga@gmail.com>
2025-02-15 08:55:19 -0300

    feat: map products in ShoppingCartMapper using ProductMapper

--- src/main/java/com/cesarfraaga/productmanagement/util/ShoppingCartMapper.java
+++ src/main/java/com/cesarfraaga/productmanagement/util/ShoppingCartMapper.java
@@ -3,11 +3,14 @@ package com.cesarfraaga.productmanagement.util;
 import com.cesarfraaga.productmanagement.dto.ShoppingCartDTO;
 import com.cesarfraaga.productmanagement.entity.ShoppingCart;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ProductMapper.class)
 public interface ShoppingCartMapper {
 
-    ShoppingCart toEntityShoppingCart(ShoppingCartDTO dto);
+    @Mapping(source = "products", target = "productsDTO")
     ShoppingCartDTO toDTOShoppingCart(ShoppingCart shoppingCart);
 
+    @Mapping(source = "productsDTO", target = "products")
+    ShoppingCart toEntityShoppingCart(ShoppingCartDTO shoppingCartDTO);
 }
