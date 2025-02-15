019d6576f7df03136f8513d1f27093c300794a83
César Fraga <cesarfraaga@gmail.com>
2025-02-15 08:54:49 -0300

    feat: add findByIdWithProducts query in ShoppingCartRepository

--- src/main/java/com/cesarfraaga/productmanagement/repository/ShoppingCartRepository.java
+++ src/main/java/com/cesarfraaga/productmanagement/repository/ShoppingCartRepository.java
@@ -2,6 +2,13 @@ package com.cesarfraaga.productmanagement.repository;
 
 import com.cesarfraaga.productmanagement.entity.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    @Query("SELECT sc FROM ShoppingCart sc LEFT JOIN FETCH sc.products WHERE sc.id = :id")
+    Optional<ShoppingCart> findByIdWithProducts(@Param("id") Long id);
+
 }
