7193028a72854ad2b73ee795eb9d4a68c79b9d08
César Fraga <cesarfraaga@gmail.com>
2025-02-19 10:25:36 -0300

    test commit

--- src/main/java/com/cesarfraaga/productmanagement/controller/ClientController.java
+++ src/main/java/com/cesarfraaga/productmanagement/controller/ClientController.java
@@ -12,7 +12,6 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "client")
 public class ClientController {
-    //teste commit
     @Autowired
     private ClientService service;
 
